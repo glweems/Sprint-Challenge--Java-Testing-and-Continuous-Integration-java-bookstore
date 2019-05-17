36590acd418663717735db352e88bf065662d9c8
Garrett Weems <gwgraphicdesign@gmail.com>
2019-05-17 11:33:30 -0400

    Author Class

--- java-bookstore/src/main/java/com/lambdaschool/bookstore/model/Author.java
+++ java-bookstore/src/main/java/com/lambdaschool/bookstore/model/Author.java
@@ -1,5 +1,47 @@
 package com.lambdaschool.bookstore.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authors")
 public class Author
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long authorid;
+
+    private String lastname;
+
+    private String firstname;
+
+    public Author() { }
+
+    public Author(String lastname, String firstname) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+    }
+
+    public long getAuthorid() {
+        return authorid;
+    }
+
+    public void setAuthorid(long authorid) {
+        this.authorid = authorid;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 }
