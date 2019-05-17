19123edaf971b6ee2785399abbb232dfb23aa1c4
Garrett Weems <gwgraphicdesign@gmail.com>
2019-05-17 11:39:33 -0400

    Book Class

--- java-bookstore/src/main/java/com/lambdaschool/bookstore/model/Book.java
+++ java-bookstore/src/main/java/com/lambdaschool/bookstore/model/Book.java
@@ -1,5 +1,58 @@
 package com.lambdaschool.bookstore.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
 public class Book
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long bookid;
+
+    private String booktitle;
+
+    private String ISBN;
+
+    private int copy;
+
+    public Book() {}
+
+    public Book(String booktitle, String ISBN, int copy) {
+        this.booktitle = booktitle;
+        this.ISBN = ISBN;
+        this.copy = copy;
+    }
+
+    public long getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(long bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getCopy() {
+        return copy;
+    }
+
+    public void setCopy(int copy) {
+        this.copy = copy;
+    }
 }
